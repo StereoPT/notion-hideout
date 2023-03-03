36a69a43eac8d750f36dc456a69c0fc1158130be
Guido <stereopt@gmail.com>
2023-03-03 12:11:33 +0000

    refactor: event scrape constants

--- src/scrapers/teatro.ts
+++ src/scrapers/teatro.ts
@@ -1,9 +1,21 @@
 import * as cheerio from "cheerio";
 import { EventType } from "../types/event.js";
 
+const EVENT = {
+  LIST: "#event-list a",
+  CATEGORY: ".entry-category",
+  TITLE: ".entry-title",
+  PLACE: ".entry-edificio",
+  DATE: {
+    WEEKDAY: ".event-date span:nth-child(1)",
+    DAY: ".event-date span:nth-child(2)",
+    MONTH: ".event-date span:nth-child(3)",
+  },
+};
+
 export const scrapeTeatro = (html: string): EventType[] => {
   const $ = cheerio.load(html);
-  const events = $("#event-list a");
+  const events = $(EVENT.LIST);
 
   console.log(` - Found ${events.length} Events`);
 
@@ -11,13 +23,13 @@ export const scrapeTeatro = (html: string): EventType[] => {
     const event = $(elem);
 
     const { id, href } = event.attr();
-    const category = event.find(".entry-category").text().trim();
-    const title = event.find(".entry-title").text().trim();
-    const place = event.find(".entry-edificio").text().trim();
+    const category = event.find(EVENT.CATEGORY).text().trim();
+    const title = event.find(EVENT.TITLE).text().trim();
+    const place = event.find(EVENT.PLACE).text().trim();
     const date = {
-      weekday: event.find(".event-date span:nth-child(1)").text().trim(),
-      day: event.find(".event-date span:nth-child(2)").text().trim(),
-      month: event.find(".event-date span:nth-child(3)").text().trim(),
+      weekday: event.find(EVENT.DATE.WEEKDAY).text().trim(),
+      day: event.find(EVENT.DATE.DAY).text().trim(),
+      month: event.find(EVENT.DATE.MONTH).text().trim(),
     };
 
     return {
