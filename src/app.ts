5674a7651def95049883de7f8296ea21473bef21
Guido <stereopt@gmail.com>
2023-05-05 10:40:05 +0100

    feat: added notion integration

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,54 @@
 import fs from "fs/promises";
 
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { Client } from "@notionhq/client";
+
 import { fetchPage } from "./utils/fetchPage.js";
 import { scrapeTeatro } from "./scrapers/teatro.js";
 
 const bannerMessage: string = "[Notion-Hideout]";
 const teatroURL: string = "https://www.teatrojlsilva.pt/";
+const databaseID: string = process.env.NOTION_DATABASE_ID;
 
 console.log(bannerMessage);
 
-const teatroHTML = await fetchPage(teatroURL);
-const scrapedEvents = scrapeTeatro(teatroHTML);
+const notion = new Client({ auth: process.env.NOTION_KEY });
+
+const addItem = async (name: string, date: string) => {
+  try {
+    await notion.pages.create({
+      parent: { database_id: databaseID },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: name,
+              },
+            },
+          ],
+        },
+        Date: {
+          date: {
+            start: new Date(date).toISOString(),
+            end: null,
+          },
+        },
+      },
+    });
+
+    console.log("Success! Entry Added!");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// addItem("Teste", "05-05-2023");
+
+// const teatroHTML = await fetchPage(teatroURL);
+// const scrapedEvents = scrapeTeatro(teatroHTML);
 
 // This Writes the Events to a JSON File:
 // await fs.writeFile("events.json", JSON.stringify(scrapedEvents, null, 2));
